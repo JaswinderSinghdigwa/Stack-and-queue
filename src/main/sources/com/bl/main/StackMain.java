0fc50bda31973512af6d838a5f02b6a385a9eec1
Aman <you@example.com>
2021-09-28 13:25:39 +0530

    StackRemoving

--- src/main/sources/com/bl/main/StackMain.java
+++ src/main/sources/com/bl/main/StackMain.java
@@ -10,9 +10,19 @@ public class StackMain {
      obj.push(70);
      obj.push(30);
      obj.push(56);
-     
+    
+     // print Stack elements
      obj.display();
-     //
+
+     // print Top element of Stack
+     System.out.printf("\nTop element is %d\n", obj.peek());
+
+     // Delete top element of Stack
+     obj.pop();
+
+     // print Stack elements
+     obj.display();
+
      // print Top element of Stack
      System.out.printf("\nTop element is %d\n", obj.peek());
  }
--- src/main/sources/com/bl/stack/Stack.java
+++ src/main/sources/com/bl/stack/Stack.java
@@ -5,13 +5,14 @@ public class Stack {
 	   public Stack() {
 	       this.top = null;
 	   }
-
+	// Utility function to add an element x in the stack
 	    public void push(int x) // insert at the beginning
 	    {
-	        
+	        // create new node temp and allocate memory
 	        Node temp = new Node();
 	 
 	        // check if stack (heap) is full. Then inserting an
+	        //  element would lead to stack overflow
 	        if (temp == null) {
 	            System.out.print("\nHeap Overflow");
 	            return;
@@ -47,7 +48,18 @@ public class Stack {
 	    }
 	 
 	    // Utility function to pop top element from the stack
-	   	public void display()
+	    public void pop() // remove at the beginning
+	    {	// check for stack underflow
+	        if (top == null) {
+	            System.out.print("\nStack Underflow");
+	            return;
+	        }
+	    	while(top!=null) {
+	        // update the top pointer to point to the next node
+	        top = (top).link;
+	    	}
+	    }
+	    public void display()
 	    {
 	        // check for stack underflow
 	        if (top == null) {
